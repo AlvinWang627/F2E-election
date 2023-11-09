7d1cf1bca0e55d5872d151d966349e7028a5d49d
Alvin <AlvinWang627@gmail.com>
2023-11-09 21:55:12 +0800

    style: style guide init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup></script>
 
 <template>
+  <div class="test">饅頭</div>
+  <h1>test</h1>
   <header></header>
   <main></main>
   <footer></footer>
 </template>
 
-<style scoped>
-
-
+<style scoped lang="scss">
+.test {
+}
 </style>
